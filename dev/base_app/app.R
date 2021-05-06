0834a885aa77ec5025198de9ae9d42c22f96595f
shambhu112 <shambhuone12@gmail.com>
2021-05-06 07:36:58 -0500

    template changes

--- dev/base_app/app.R
+++ dev/base_app/app.R
@@ -1,116 +1,135 @@
-#
-# This is a Shiny web app based on bs4Dash template
-# the 'Run App' button above.
+## This Shiny App created with Shinyspring - http://www.shinyspring.dev
+## template : bs4Dash
+## app_type : minimal
 
 library(shiny)
 library(bs4Dash)
 library(thematic)
 library(waiter)
+library(stringr)
+library(dplyr)
 library(sweetmods)
-#source("dashcomponents.R")
-#source("util.R")
 
 thematic_shiny()
 
-params <- config::get()
-controller <- sweetmods::app_master$new(params)
 
 
-# Define UI for application that draws a histogram
-ui <- bs4Dash::dashboardPage(
-    preloader = list(
-        waiter = list(html = tagList(spin_1(), "Loading ..."), color = "#343a40"),
-        duration = 0
-    ),
+params <- config::get(file = "config.yml") ## @@sweetmod_config
+controller <- app_master$new(params)
+controller$preload_master_with_config()
+registry <- sweetmods::mod_registry$new(params)
+mod_names <- registry$mods_names()
 
-    dark = TRUE,
-    help = FALSE,
-    fullscreen = TRUE,
-    scrollToTop = TRUE,
-    header = dashboardHeader(
-        title = dashboardBrand(
-            title = "ShinySpring",
-            color = "primary",
-            href = "http://www.shinyspring.dev",
-            image = "https://pbs.twimg.com/profile_images/935874269199904768/qeTpzKUe.jpg",
-            opacity = 0.8
-        ),
-        fixed = TRUE,
-        rightUi = tagList(
-            dropdownMenu(
-                badgeStatus = "info",
-                type = "notifications",
-                messageItem(
-                    inputId = "triggerAction1",
-                    message = "FDIC issues list of Bank Examined for CRA compliance",
-                    from = "FDIC",
-                    image = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Seal_of_the_United_States_Federal_Deposit_Insurance_Corporation.svg/1200px-Seal_of_the_United_States_Federal_Deposit_Insurance_Corporation.svg.png",
-                    time = "today",
-                    color = "lime"
-                )
-            ),
-            userOutput("user")
-        ),
-        leftUi = tagList(
-            # Close dropdownMenu
-            tags$li(class = "dropdown",
-                    tags$h3("Bank Branch Optimizer")
-            )
-        ) # close left UI
-    ),
-    sidebar = dashboardSidebar(
-        fixed = TRUE,
-        skin = "light",
-        status = "primary",
-        id = "sidebar",
-        customArea = fluidRow(
-            actionButton(
-                inputId = "myAppButton",
-                label = NULL,
-                icon = icon("users"),
-                width = NULL,
-                status = "primary",
-                style = "margin: auto",
-                dashboardBadge(textOutput("btnVal"), color = "danger")
-            )
-        ),
-        sidebarUserPanel(
-            image = "https://image.flaticon.com/icons/svg/1149/1149168.svg",
-            name = "Welcome"
-        ),
-        sidebarMenu(
-            id = "current_tab",
-            flat = FALSE,
-            compact = FALSE,
-            childIndent = TRUE,
-            sidebarHeader("Banks"),
-            menuItem(
-                "Branch Network",
-                tabName = "institutions_tab",
-                icon = icon("university")
-            ),
-            menuItem(
-                "Branch Analysis",
-                tabName = "branches_tab",
-                icon = icon("piggy-bank")
-            )
+# call on_load function on all modules
+onl <- sapply(mod_names, function(x){
+  p <- registry$params_for_mod(x)
+  package_prefix <- ifelse("package" %in% names(p) , paste0(p$package , "::") , "" )
+  if("onload_function" %in% names(p)){
+    onload_f <- paste0(package_prefix , p$onload_function , "(controller ,params = p)")
+    eval(parse(text= onload_f))
+    cli::cli_alert_info("Executed onload for {x} : {onload_f}")
+  }
+})
+onl <- NULL
 
-        )
-    ), # Close of sidebar
-    body = dashboardBody(
-        tabItems(
-            tabItem(tabName = "learning_tab", "Learning"),
-   #         tabItem(tabName = "institutions_tab", branch_network_ui("b_network_ui" , control)),
-            tabItem(tabName = "credits_tab", "Credits")
-
-        )
-    ) #close of body
-)
-
-# Define server logic required to draw a histogram
-server <- function(input, output , session) {
-
+# Function to create the tab item and call the ui_function in module
+create_tab_module <- function(tab_module){
+  p <- registry$params_for_mod(tab_module)
+  ui_function <-  p$ui_function
+  package_prefix <- ifelse("package" %in% names(p) , paste0(p$package , "::") , "" )
+ tabItem(
+    tabName = tab_module,
+    eval(parse(text = paste0(package_prefix , ui_function , "(id = '" , tab_module  ,"' , control = controller ,params = p)"  )))
+  )
 }
 
-# Run the application
-shinyApp(ui = ui, server = server)
+
+
+## Define UI
+ui <- bs4Dash::dashboardPage(
+
+  dark = FALSE , ## FALSE,
+  help = FALSE,
+  fullscreen = TRUE,
+  scrollToTop = TRUE,
+  header = dashboardHeader(
+    title = dashboardBrand(
+      title = "Sample App",
+      color = "primary", ## @@title_color
+      href = "https://www.shinyspring.dev", ## @@ header_href
+      image = "https://storage.googleapis.com/shiny-pics/spring_logo.png", ## @@header_image
+      opacity = 0.8
+    ),
+    fixed = TRUE , ## @@header_fixed
+    leftUi = tagList(
+      ## Title Text here
+      tags$li(class = "dropdown",
+              tags$h3("BS4 Dash Minimalistic") ## @@app_title_h3
+      )
+    ) ## close left UI
+  ),
+  sidebar = dashboardSidebar(
+    fixed = TRUE, ## @@side_bar_fixed
+    skin = "light",
+    status = "primary",
+    id = "sidebar",
+
+    sidebarMenu(
+      id = "current_tab",
+      flat = FALSE,
+      compact = FALSE,
+      childIndent = TRUE,
+      sidebarHeader("Sample App"),
+
+# Whisker:  Menus
+        menuItem(
+          "Introduction" ,
+          tabName = "intro_tab",
+          icon = icon("university")
+        ),
+        menuItem(
+          "Core Analysis" ,
+          tabName = "core_tab",
+          icon = icon("indent")
+        ),
+        menuItem(
+          "Data Exploration" ,
+          tabName = "explore_tab",
+          icon = icon("chart-bar")
+        ),
+        menuItem(
+          "Credits" ,
+          tabName = "credits_tab",
+          icon = icon("heart")
+        )
+          )
+    ),  ## Close of sidebar
+  body = dashboardBody(
+    tabItems(
+      create_tab_module(tab_module = "intro_tab") ,
+      create_tab_module(tab_module = "core_tab") ,
+      create_tab_module(tab_module = "explore_tab") ,
+      create_tab_module(tab_module = "credits_tab") 
+      )
+    ) # Close of tab items
+)
+
+
+
+## Define server logic required to draw a histogram
+server <- function(input, output , session) {
+  mods <- registry$mods_names()
+  for(i in 1:length(mods)){
+    id <- mods[i]
+    p <- registry$params_for_mod(id)
+    index <- which(names(controller$params) == paste0(id , ".server_function"))
+  if(length(index) > 0 ){
+      server_function <- controller$params[index]
+      eval(parse(text= paste0(server_function , "(id = '" , id , "' , control = controller , params = p)")))
+     }
+  }
+  }
+
+    ## Run the application
+    shinyApp(ui = ui, server = server)
+
